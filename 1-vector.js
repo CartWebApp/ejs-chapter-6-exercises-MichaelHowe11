18e9e851297cb0c6a8f709fa5f4ec9e7510e737c
MichaelHowe11 <michael.howell23@student.cart.org>
2024-02-20 15:30:30 -0800

    vector

--- 1-vector.js
+++ 1-vector.js
@@ -7,10 +7,45 @@
 // Your code here.
 
 
+class Vec {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    get length() { return (Math.sqrt(this.x ** 2 + this.y ** 2)) }
+
+    plus(addvec) {
+
+        this.y += addvec.y;
+
+        this.x += addvec.x;
+
+
+        return (this);
+
+
+    }
+
+    minus(minusvec) {
+        this.y -= minusvec.y
+
+        this.x -= minusvec.x
+
+        return (this);
+
+    }
+
+}
+
+
+
+// console.log(new Vec(3, 2).plus(2, 3))
+
 // Tests:
 console.log(new Vec(1, 2).plus(new Vec(2, 3)));
 // → Vec{x: 3, y: 5}
 console.log(new Vec(1, 2).minus(new Vec(2, 3)));
-// → Vec{x: -1, y: -1}
+// // → Vec{x: -1, y: -1}
 console.log(new Vec(3, 4).length);
 // → 5
